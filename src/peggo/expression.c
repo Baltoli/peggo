bc6f0b3f60a48eeb817418ec377f4fcd0bb8cf98
baltoli <bsc28@cam.ac.uk>
2017-01-06 13:48:46 +0000

    Use memory

--- src/peggo/expression.c
+++ src/peggo/expression.c
@@ -1,12 +1,18 @@
+#include <stdlib.h>
+
 #include "expression.h"
 
-peg_expr_t node_init() {
-  peg_expr_t node;
+peg_expr_t *peg_init() {
+  peg_expr_t *node = malloc(sizeof(*node));
+  node->type = Node_Invalid;
+  node->data = NULL;
+  node->left = NULL;
+  node->right = NULL;
   return node;
 }
 
-peg_expr_t node_empty() {
-  peg_expr_t node = node_init();
-  node.type = Node_Empty;
+peg_expr_t *peg_empty() {
+  peg_expr_t *node = peg_init();
+  node->type = Node_Empty;
   return node;
 }
--- src/peggo/expression.h
+++ src/peggo/expression.h
@@ -11,14 +11,18 @@ typedef enum peg_expr_node_en {
   Node_One_Or_More,
   Node_Optional,
   Node_And,
-  Node_Or
+  Node_Or,
+  Node_Invalid
 } peg_expr_node_t;
 
 typedef struct peg_expr_st {
   enum peg_expr_node_en type;
+  char *data;
+  struct peg_expr_st *left;
+  struct peg_expr_st *right;
 } peg_expr_t;
 
-peg_expr_t node_init();
-peg_expr_t node_empty();
+peg_expr_t *peg_init();
+peg_expr_t *peg_empty();
 
 #endif
