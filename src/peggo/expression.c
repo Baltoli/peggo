0e193f22acc97fba070a3817ef6b853bb6ade9f6
baltoli <bsc28@cam.ac.uk>
2017-01-06 15:21:37 +0000

    More constructors

--- src/peggo/expression.c
+++ src/peggo/expression.c
@@ -3,9 +3,9 @@
 
 #include "expression.h"
 
-peg_expr_t *peg_init() {
+peg_expr_t *peg_init(peg_expr_node_t type) {
   peg_expr_t *node = malloc(sizeof(*node));
-  node->type = Node_Invalid;
+  node->type = type;
   node->data = NULL;
   node->left = NULL;
   node->right = NULL;
@@ -30,16 +30,68 @@ void peg_free(peg_expr_t *node) {
   }
 }
 
+void peg_init_data(peg_expr_t *node, const char *s) {
+  if(node->data) {
+    free(node->data);
+  }
+
+  node->data = malloc((sizeof(char) * strlen(s)) + 1);
+  strcpy(node->data, s);
+}
+
+void peg_init_left(peg_expr_t *node, peg_expr_t *left) {
+  if(node->left) {
+    peg_free(node->left);
+  }
+
+  node->left = left;
+}
+
+void peg_init_right(peg_expr_t *node, peg_expr_t *right) {
+  if(node->right) {
+    peg_free(node->right);
+  }
+
+  node->right = right;
+}
+
 peg_expr_t *peg_empty() {
-  peg_expr_t *node = peg_init();
-  node->type = Node_Empty;
-  return node;
+  return peg_init(Node_Empty);
 }
 
 peg_expr_t *peg_terminal(const char *t) {
-  peg_expr_t *node = peg_init();
-  node->type = Node_Terminal;
-  node->data = malloc((sizeof(char) * strlen(t)) + 1);
-  strcpy(node->data, t);
+  peg_expr_t *node = peg_init(Node_Terminal);
+  peg_init_data(node, t);
+  return node;
+}
+
+peg_expr_t *peg_non_terminal(const char *nt) {
+  peg_expr_t *node = peg_init(Node_Non_Terminal);
+  peg_init_data(node, nt);
+  return node;
+}
+
+peg_expr_t *peg_sequence(peg_expr_t *expr) {
+  peg_expr_t *node = peg_init(Node_Sequence);
+  peg_init_left(node, expr);
+  return node;
+}
+
+peg_expr_t *peg_zero_or_more(peg_expr_t *expr) {
+  peg_expr_t *node = peg_init(Node_Zero_Or_More);
+  peg_init_left(node, expr);
+  return node;
+}
+
+peg_expr_t *peg_one_or_more(peg_expr_t *expr) {
+  peg_expr_t *node = peg_init(Node_One_Or_More);
+  peg_init_left(node, expr);
+  return node;
+}
+
+peg_expr_t *peg_choice(peg_expr_t *left, peg_expr_t *right) {
+  peg_expr_t *node = peg_init(Node_Choice);
+  peg_init_left(node, left);
+  peg_init_right(node, right);
   return node;
 }
--- src/peggo/expression.h
+++ src/peggo/expression.h
@@ -2,17 +2,17 @@
 #define EXPRESSION_H
 
 typedef enum peg_expr_node_en {
+  Node_Invalid = 0,
+  Node_Empty,
   Node_Terminal,
   Node_Non_Terminal,
-  Node_Empty,
   Node_Sequence,
   Node_Choice,
   Node_Zero_Or_More,
   Node_One_Or_More,
   Node_Optional,
   Node_And,
-  Node_Or,
-  Node_Invalid
+  Node_Not
 } peg_expr_node_t;
 
 typedef struct peg_expr_st {
@@ -22,9 +22,13 @@ typedef struct peg_expr_st {
   struct peg_expr_st *right;
 } peg_expr_t;
 
-peg_expr_t *peg_init();
 void peg_free(peg_expr_t *node);
 peg_expr_t *peg_empty();
 peg_expr_t *peg_terminal(const char *t);
+peg_expr_t *peg_non_terminal(const char *nt);
+peg_expr_t *peg_sequence(peg_expr_t *expr);
+peg_expr_t *peg_zero_or_more(peg_expr_t *expr);
+peg_expr_t *peg_one_or_more(peg_expr_t *expr);
+peg_expr_t *peg_choice(peg_expr_t *left, peg_expr_t *right);
 
 #endif
