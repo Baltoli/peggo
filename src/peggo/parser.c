b6112d31e75e3e1c7abbdc9533a7d5b358f6ff6e
baltoli <bsc28@cam.ac.uk>
2017-01-08 11:09:42 +0000

    Structure of parser

--- src/peggo/parser.c
+++ src/peggo/parser.c
@@ -4,6 +4,15 @@
 #include "parser.h"
 
 parse_t *parse(char *source, grammar_t *grammar) {
+  // TODO
+  parse_t *parent = parse_init("Start", 0, 0);
+  return parse_dispatch(source, NULL, 0, parent);
+}
+
+parse_t *parse_dispatch(char *source, expr_t *rule, size_t start, parse_t *parent) {
+  // TODO
+  switch(rule->type) {
+  }
   return NULL;
 }
 
@@ -36,4 +45,23 @@ parse_t *parse_terminal(char *source, char *symbol, size_t start, parse_t *paren
 
   free(annotated);
   return ret;
+}
+
+parse_t *parse_non_terminal(char *source, char *symbol, size_t start, parse_t *parent) {
+  // TODO: this needs to do quite a lot of work:
+  //        - Needs to get the rule from somewhere. We have the grammar at the
+  //          start of parsing, but this would need to be passed around a lot.
+  //          One option is to make the parser non-thread safe and just have a
+  //          static variable in the parser that contains a mapping from symbols
+  //          to rules. This is also the point at which the grammar could be
+  //          checked for validity (every non-terminal used needs a production
+  //          rule).
+  //        - Given a rule, needs to parse it. We should write a generic
+  //          dispatcher for expression node types. Parsing a sub-node of an
+  //          expression will basically amount to looking up what the type is,
+  //          then calling the appropriate parsing methods with the newly
+  //          constructed parent node.
+  //        - Remember: no other node type should modify the recursive parameter
+  //          parent, only non-terminal. All others end up in a flat structure.
+  return NULL;
 }
--- src/peggo/parser.h
+++ src/peggo/parser.h
@@ -5,7 +5,9 @@
 #include "parse_tree.h"
 
 parse_t *parse(char *source, grammar_t *grammar);
+parse_t *parse_dispatch(char *source, expr_t *rule, size_t start, parse_t *parent);
 parse_t *parse_empty(char *source, size_t start, parse_t *parent);
 parse_t *parse_terminal(char *source, char *symbol, size_t start, parse_t *parent);
+parse_t *parse_non_terminal(char *source, char *symbol, size_t start, parse_t *parent);
 
 #endif
