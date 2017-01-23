63edfa0a015e74241b4e6e633cf5b5e4f8db60eb
baltoli <bsc28@cam.ac.uk>
2017-01-23 10:41:28 +0000

    Add build option for log level, fix placeholder

--- src/lib/log.c
+++ src/lib/log.c
@@ -3,6 +3,21 @@
 
 #include "log.h"
 
+char *level_to_string(unsigned int level) {
+  switch(level) {
+    case 1:
+      return "error";
+    case 2:
+      return "warning";
+    case 3:
+      return "info";
+    case 4:
+      return "debug";
+    default:
+      return "invalid log level";
+  }
+}
+
 void fatal_error(char *message) {
   fprintf(stderr, "libpeggo fatal error: %s\n", message);
   exit(EXIT_FAILURE);
@@ -10,6 +25,6 @@ void fatal_error(char *message) {
 
 void log_level(unsigned int level, char *message) {
   if(level <= DEBUG_LEVEL) {
-    fprintf(stderr, "libpeggo [%s]: %s\n", "placeholder", message);
+    fprintf(stderr, "libpeggo [%s]: %s\n", level_to_string(level), message);
   }
 }
