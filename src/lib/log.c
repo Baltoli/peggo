339f403289169311321a8f22bf78f0c94c0a9314
baltoli <bsc28@cam.ac.uk>
2017-01-23 10:06:54 +0000

    Add generic leveled logging function

--- src/lib/log.c
+++ src/lib/log.c
@@ -7,3 +7,9 @@ void fatal_error(char *message) {
   fprintf(stderr, "libpeggo fatal error: %s\n", message);
   exit(EXIT_FAILURE);
 }
+
+void log_level(unsigned int level, char *message) {
+  if(level <= DEBUG_LEVEL) {
+    fprintf(stderr, "libpeggo [%s]: %s\n", "placeholder", message);
+  }
+}
--- src/lib/log.h
+++ src/lib/log.h
@@ -1,6 +1,10 @@
 #ifndef LOG_H
 #define LOG_H
 
+#ifndef DEBUG_LEVEL
+#define DEBUG_LEVEL 0
+#endif
+
 /**
  * Logs a message to `stderr`, then exits the program with a nonzero return
  * code.
@@ -8,5 +12,14 @@
  * \param message The message to log
  */
 void fatal_error(char *message);
+
+/**
+ * Log a message to `stderr` at the given level. If the level is less than \ref
+ * DEBUG_LEVEL as defined at compile time, then the message will not be logged.
+ *
+ * \param level The debug level at which to log this message
+ * \param message The message to log
+ */
+void log_level(unsigned int level, char *message);
 
 #endif
