e57829ff76311842b0862a1fcef254a927248240
baltoli <bsc28@cam.ac.uk>
2017-02-19 21:01:45 +0000

    Use new API in whitespace tests

--- test/test_parser_whitespace.c
+++ test/test_parser_whitespace.c
@@ -9,10 +9,11 @@ void whitespace_none(void **state) {
     ), 1
   );
 
-  parse_t *result = parse("none", grammar);
+  parse_result_t *result = parse("none", grammar);
   assert_non_null(result);
+  assert_true(is_success(result));
 
-  assert_int_equal(result->length, 0);
+  assert_int_equal(result->data.result->length, 0);
 }
 
 void whitespace_spaces(void **state) {
@@ -24,11 +25,13 @@ void whitespace_spaces(void **state) {
     ), 1
   );
 
-  parse_t *result = parse("    ", grammar);
+  parse_result_t *result = parse("    ", grammar);
   assert_non_null(result);
+  assert_true(is_success(result));
 
-  assert_int_equal(result->length, 4);
-  assert_int_equal(result->n_children, 4);
+  parse_t *suc = result->data.result;
+  assert_int_equal(suc->length, 4);
+  assert_int_equal(suc->n_children, 4);
 }
 
 void whitespace_tabs(void **state) {
@@ -40,11 +43,13 @@ void whitespace_tabs(void **state) {
     ), 1
   );
 
-  parse_t *result = parse("\t\t", grammar);
+  parse_result_t *result = parse("\t\t", grammar);
   assert_non_null(result);
+  assert_true(is_success(result));
 
-  assert_int_equal(result->length, 2);
-  assert_int_equal(result->n_children, 2);
+  parse_t *suc = result->data.result;
+  assert_int_equal(suc->length, 2);
+  assert_int_equal(suc->n_children, 2);
 }
 
 void whitespace_mixed(void **state) {
@@ -56,11 +61,13 @@ void whitespace_mixed(void **state) {
     ), 1
   );
 
-  parse_t *result = parse(" \t \t ", grammar);
+  parse_result_t *result = parse(" \t \t ", grammar);
   assert_non_null(result);
+  assert_true(is_success(result));
 
-  assert_int_equal(result->length, 5);
-  assert_int_equal(result->n_children, 5);
+  parse_t *suc = result->data.result;
+  assert_int_equal(suc->length, 5);
+  assert_int_equal(suc->n_children, 5);
 }
 
 void whitespace_newlines(void **state) {
@@ -72,9 +79,11 @@ void whitespace_newlines(void **state) {
     ), 1
   );
 
-  parse_t *result = parse("  \n\t", grammar);
+  parse_result_t *result = parse("  \n\t", grammar);
   assert_non_null(result);
+  assert_true(is_success(result));
 
-  assert_int_equal(result->length, 2);
-  assert_int_equal(result->n_children, 2);
+  parse_t *suc = result->data.result;
+  assert_int_equal(suc->length, 2);
+  assert_int_equal(suc->n_children, 2);
 }
